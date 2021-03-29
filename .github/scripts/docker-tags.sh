192e50d1b5ffe219ef89dbd610175979ddf2352b
Leonid Makarov <leonid.makarov@ffwagency.com>
2021-03-29 11:41:06 -0700

    Debug

--- .github/scripts/docker-tags.sh
+++ .github/scripts/docker-tags.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
+set -xeuo pipefail
 
 # Generates docker images tags for the docker/build-push-action@v2 action depending on the branch/tag.
 
+# Config
+IMAGE_TAG_BASE="${IMAGE}:php${VERSION}"
+
+# Registries
 declare -a registryArr
 registryArr+=("docker.io") # Docker Hub
 registryArr+=("ghcr.io") # GitHub Container Registry
 
+# Image tags
 declare -a imageTagArr
 
-# feature/* => sha-xxxxxxx
-# Note: disabled
-#if [[ "${GITHUB_REF}" =~ "refs/heads/feature/" ]]; then
-#	GIT_SHA7=$(echo ${GITHUB_SHA} | cut -c1-7) # Short SHA (7 characters)
-#	imageTagArr+=("${IMAGE}:php${VERSION}-sha-${GIT_SHA7}")
-#fi
+# On every build
+GIT_SHORT_SHA="${GITHUB_SHA:1:7}" # Short SHA (7 characters)
+imageTagArr+=("${IMAGE_TAG_BASE}-build-${GIT_SHORT_SHA}")
 
 # develop => version-edge
 if [[ "${GITHUB_REF}" == "refs/heads/develop" ]]; then
-	imageTagArr+=("${IMAGE}:php${VERSION}-edge")
+	imageTagArr+=("${IMAGE_TAG_BASE}-edge")
 fi
 
 # master => version
 if [[ "${GITHUB_REF}" == "refs/heads/master" ]]; then
-	imageTagArr+=("${IMAGE}:php${VERSION}")
+	imageTagArr+=("${IMAGE_TAG_BASE}")
 fi
 
 # tags/v1.0.0 => 1.0
@@ -31,9 +34,9 @@ if [[ "${GITHUB_REF}" =~ "refs/tags/" ]]; then
 	IFS='.' read -a release_arr <<< "${GITHUB_REF#refs/tags/}"
 	releaseMajor=${release_arr[0]#v*}  # 2.7.0 => "2"
 	releaseMinor=${release_arr[1]}  # "2.7.0" => "7"
-	imageTagArr+=("${IMAGE}:php${VERSION}")
-	imageTagArr+=("${IMAGE}:php${VERSION}-${releaseMajor}")
-	imageTagArr+=("${IMAGE}:php${VERSION}-${releaseMajor}.${releaseMinor}")
+	imageTagArr+=("${IMAGE_TAG_BASE}")
+	imageTagArr+=("${IMAGE_TAG_BASE}-${releaseMajor}")
+	imageTagArr+=("${IMAGE_TAG_BASE}-${releaseMajor}.${releaseMinor}")
 fi
 
 # Build an array of registry/image:tag values
